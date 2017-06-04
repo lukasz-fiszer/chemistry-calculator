326d215e89f8e729f12cb64c574ecc2210de86fa
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2017-06-04 21:02:21 +0200

    Add footer to the layout template
    Add footer to the layout template, is now included there
    Add layout.footer template
    Add public/css/style.css file that contains some css added for sticky footer
    Layout template change, now it has wrapper div of class body-content between layout.nav and layout.footer where content section is yielded

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,12 +10,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="author" content="Łukasz Fiszer">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
 @endsection
 
 @section('body')
 	@include('layout.nav')
 
-	@yield('content')
-	
+	<div class="body-content">
+		@yield('content')
+	</div>
+
+	@include('layout.footer')
+
 	@yield('footer_elements')
 @endsection
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	<div class="container">
+		<div class="content has-text-right">
+			<p><b>Chemistry Calculator</b> - description</p>
+			<p>Made by Łukasz Fiszer</p>
+		</div>
+	</div>
+</footer>
