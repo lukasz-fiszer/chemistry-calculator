5ee11c0b92c19f385ff0e207699b5465cacd4cff
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2017-09-02 02:29:43 +0200

    Clean layout template
    Clean layout template, layout.nav template
    Clean hamburger.js
    Edit webpack.mix.js, scroll.js is not included for layout.js file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,6 @@
 	</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="author" content="Łukasz Fiszer">
-	{{-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css"> --}}
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css">
 	<link rel="stylesheet" type="text/css" href="{{ mix('/css/style.css') }}">
 @endsection
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,19 +1,3 @@
-{{-- <nav class="nav has-shadow">
-	<div class="container">
-		<div class="nav-left">
-			<a href="/" class="nav-item">Chemistry Calculator</a>
-		</div>
-		<span class="nav-toggle">
-			<span></span>
-			<span></span>
-			<span></span>
-		</span>
-		<div class="nav-right nav-menu">
-			<a href="/home" class="nav-item is-tab is-active">Home</a>
-			<a href="/about" class="nav-item is-tab">About</a>
-		</div>
-	</div>
-</nav> --}}
 <nav class="navbar has-shadow">
 	<div class="navbar-brand">
 		<a href="/" class="navbar-item">Chemistry Calculator</a>
