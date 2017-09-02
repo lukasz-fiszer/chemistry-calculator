749645c795f39fc3bf2fd9b535db7d672479baeb
Łukasz Fiszer <45523649+lukasz-fiszer@users.noreply.github.com>
2017-09-02 02:09:33 +0200

    Edit layout.nav template
    Edit layout.nav template, edit nav element to use navbar
    Edit layout template, edit css lib version used
    Edit layout/hamburger.js, edit navbar elements used

--- resources/assets/js/layout/hamburger.js
+++ resources/assets/js/layout/hamburger.js
@@ -1,6 +1,8 @@
 (function() {
-	var burger = document.querySelector('.nav-toggle');
-	var menu = document.querySelector('.nav-menu');
+	/*var burger = document.querySelector('.nav-toggle');
+	var menu = document.querySelector('.nav-menu');*/
+	var burger = document.querySelector('.navbar-burger');
+	var menu = document.querySelector('.navbar-menu');
 	burger.addEventListener('click', function() {
 		burger.classList.toggle('is-active');
 		menu.classList.toggle('is-active');
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="nav has-shadow">
+{{-- <nav class="nav has-shadow">
 	<div class="container">
 		<div class="nav-left">
 			<a href="/" class="nav-item">Chemistry Calculator</a>
@@ -11,6 +11,22 @@
 		<div class="nav-right nav-menu">
 			<a href="/home" class="nav-item is-tab is-active">Home</a>
 			<a href="/about" class="nav-item is-tab">About</a>
+		</div>
+	</div>
+</nav> --}}
+<nav class="navbar has-shadow">
+	<div class="navbar-brand">
+		<a href="/" class="navbar-item">Chemistry Calculator</a>
+		<span class="navbar-burger">
+			<span></span>
+			<span></span>
+			<span></span>
+		</span>
+	</div>
+	<div class="navbar-menu">
+		<div class="navbar-end">
+			<a href="/home" class="navbar-item is-tab is-active">Home</a>
+			<a href="/about" class="navbar-item is-tab">About</a>
 		</div>
 	</div>
 </nav>
